92e650e5672f84abcfe4764611572b27de44da86
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-04-08 11:26:42 +0300

    feat: api working

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,11 +1,44 @@
-import React from "react";
+import React from 'react';
+
+interface imageLinksObj {
+    smallThumbnail?: string;
+    thumbnail?: string;
+}
+type ItemType = {
+    title: string;
+    subtitle: string;
+    authors?: string[];
+    publisher: string;
+    publishedDate: string;
+    description: string;
+    industryIdentifiers: object[];
+    readingModes: object;
+    pageCount: number;
+    printType: string;
+    categories: string[];
+    averageRating: number;
+    ratingsCount: number;
+    maturityRating: string;
+    allowAnonLogging: boolean;
+    contentVersion: string;
+    panelizationSummary: object;
+    imageLinks: imageLinksObj;
+    language: string;
+    previewLink: string;
+    infoLink: string;
+    canonicalVolumeLink: string;
+    id: string;
+    shelf?: string;
+};
 interface BookProps {
-    item: any;
+    item: ItemType;
     key: number;
     handleShelfChange: Function;
 }
 export const Book = ({item, handleShelfChange}: BookProps) => {
-    const {title, authors, publishedDate, imageLinks, shelf} = item;
+    const {
+        title, authors, imageLinks, shelf
+    } = item;
     const handleChange = (e: any) => {
         const newShelf = e.target.value;
         const objectTitle = title;
@@ -14,17 +47,18 @@ export const Book = ({item, handleShelfChange}: BookProps) => {
     return (
         <div className="book">
             <div className="book-thumbnail-container">
-                <img src={imageLinks.smallThumbnail} className="book-thumbnail" alt="book thumbnail"></img>
+                <img src={imageLinks.smallThumbnail} className="book-thumbnail" alt="book thumbnail" />
             </div>
-
-            <p className="book-title">{`${title} ${publishedDate}`}</p>
-            <div className="book-authors">
-                {authors.map((author: string, index: number) => (
-                    <p key={index}> {author}</p>
-                ))}
-            </div>
+            <p className="book-title">{title}</p>
+            {authors && (
+                <div className="book-authors">
+                    {authors.map((author: string, index: number) => (
+                        <p key={index}> {author}</p>
+                    ))}
+                </div>
+            )}
             <div className="book-selector">
-                <select value={shelf} onChange={handleChange}>
+                <select value={shelf || 'none'} onChange={handleChange}>
                     <option value="move" disabled>
                         Move to...
                     </option>
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,51 +1,60 @@
-import React, {useState, useEffect} from "react";
-import {Book} from "./Book";
-import {FaAngleLeft, FaSearch} from "react-icons/fa";
-import {Link} from "react-router-dom";
-import * as BooksAPI from "../BooksAPI";
+import React, {useState, useEffect} from 'react';
+import {FaAngleLeft, FaSearch} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
+import {Book} from './Book';
+import * as BooksAPI from '../BooksAPI';
+
 interface SearchProps {
     books: object[];
     setBooks: Function;
 }
 export const Search = ({books, setBooks}: SearchProps) => {
-    const [query, setQuery] = useState("");
-    const queryResult = books.filter((book: any) => book.title.toLowerCase().includes(query.toLowerCase()));
-    useEffect(() => {}, [query]);
+    const [query, setQuery] = useState('');
+    const [searchResult, setSearchResult] = useState([]);
+    // const queryResult = searchResult.filter((book: any) => book.title.toLowerCase().includes(query.toLowerCase()));
+    useEffect(() => {
+        fetchData();
+    }, [query]);
+    const fetchData = async () => {
+        const data = await BooksAPI.search(query);
+        data !== undefined && setSearchResult(data);
+        console.log('search result', searchResult);
+        console.log(typeof searchResult);
+    };
     const handleShelfChange = (newValue: string, objectToMove: string, item: object) => {
-        // const newBooks = [...books];
-        // const obj: any = newBooks.find((book: any) => book.title === objectToMove);
-        // const index = newBooks.map((e: any) => e.title).indexOf(objectToMove);
-        // if (obj) {
-        //     obj.shelf = newValue;
-        //     newBooks[index] = obj;
-        //     setBooks(newBooks);
-        // }
+        const newBooks = [...books];
+        const obj: any = newBooks.find((book: any) => book.title === objectToMove);
+        const index = newBooks.map((e: any) => e.title).indexOf(objectToMove);
+        if (obj) {
+            obj.shelf = newValue;
+            newBooks[index] = obj;
+            setBooks(newBooks);
+        }
     };
     return (
         <>
             <div className="search-header">
                 <Link to="/" className="nav-link">
-                    <FaAngleLeft className="icons"></FaAngleLeft>
+                    <FaAngleLeft className="icons" />
                 </Link>
                 <div className="search-input-container">
-                    <input
-                        type="text"
-                        placeholder="Search Book"
-                        value={query}
-                        onChange={(event) => setQuery(event.target.value)}
-                        className="search-input"
-                    ></input>
-                    <FaSearch className="icons"></FaSearch>
+                    <input type="text" placeholder="Search Book" value={query} onChange={(event) => setQuery(event.target.value)} className="search-input" />
+                    <FaSearch className="icons" />
                 </div>
 
-                <div></div>
+                <div />
             </div>
-
-            <div className="search-result">
-                {queryResult.map((item: any, index) => (
-                    <Book item={item} key={index} handleShelfChange={handleShelfChange}></Book>
-                ))}
-            </div>
+            {/* <div className="search-result">
+                {searchResult ? (
+                    <div style={{color: 'white'}}>
+                        {searchResult.map((item: any, index) => (
+                            <Book item={item} key={index} handleShelfChange={handleShelfChange} />
+                        ))}
+                    </div>
+                ) : (
+                    <div style={{color: 'white'}}>No Result</div>
+                )}
+            </div> */}
         </>
     );
 };
